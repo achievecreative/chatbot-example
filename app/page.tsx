989227b08115439033c955a4c488919c313a1ec6
devs <hi@achievecreative.com>
2025-11-21 23:10:26 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Chat from "@/components/Chat"
 export default async function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col py-32 bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col py-4 bg-white dark:bg-black sm:items-start">
         <Chat />
       </main>
     </div>
