30642815ab0d35d23621eb14a42c36a5dad6314f
devs <hi@achievecreative.com>
2025-11-29 10:55:50 +0800

    add the animation when open the panel

--- components/ChatbotWidget.tsx
+++ components/ChatbotWidget.tsx
@@ -2,17 +2,57 @@
 
 import { cn } from "@/lib/utils"
 import Image from "next/image"
-import React from "react"
+import React, { useEffect, useRef } from "react"
 import Chat from "./Chat"
 
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react"
+
+gsap.registerPlugin(useGSAP)
+
 export default function ChatbotWidget() {
+  const containerRef = useRef<HTMLDivElement>(null)
+
   const [showChatWidget, setShowChatWidget] = React.useState(false)
+
+  const { contextSafe } = useGSAP(() => {}, { scope: containerRef })
+
+  const openChatPanel = contextSafe(() => {
+    gsap.to(".chat-panel", {
+      duration: 0.6,
+      scale: 1, // expand to full size
+      opacity: 1,
+      ease: "back.out(1.4)",
+    })
+  })
+
+  const closeChatPanel = contextSafe(() => {
+    gsap.to(".chat-panel", {
+      duration: 0.6,
+      scale: 0, // shrink to zero size
+      opacity: 0,
+      ease: "back.in(1.4)",
+    })
+  })
+
+  const widgetClicked = () => {
+    setShowChatWidget(!showChatWidget)
+  }
+
+  useEffect(() => {
+    if (showChatWidget) {
+      openChatPanel()
+    } else {
+      closeChatPanel()
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [showChatWidget])
+
   return (
-    <div>
+    <div ref={containerRef}>
       <div
         className={cn(
-          "fixed max-h-screen h-[calc(100vh-130px)] right-10 bottom-24 lg:w-[calc(100vw-420px)]  max-w-full p-4 border-0 rounded-lg shadow-lg",
-          showChatWidget ? "" : "hidden"
+          "chat-panel fixed max-h-screen h-[calc(100vh-130px)] right-10 bottom-24 lg:w-[calc(100vw-420px)]  max-w-full p-4 border border-gray-300 rounded-lg shadow-2xl scale-0 opacity-0 transform"
         )}
       >
         <div className="flex min-h-full w-full flex-col bg-white dark:bg-black sm:items-start">
@@ -26,7 +66,7 @@ export default function ChatbotWidget() {
           width={65}
           height={65}
           className="m-1"
-          onClick={() => setShowChatWidget(!showChatWidget)}
+          onClick={widgetClicked}
         />
       </div>
     </div>
